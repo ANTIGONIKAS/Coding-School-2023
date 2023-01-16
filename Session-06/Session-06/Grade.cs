99d9f9864673c3d9661ac5949b88a3371fb3ffa4
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-16 17:54:21 +0200

    grade class

---
+++ Session-06/Session-06/Grade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Grade
+{
+    public Guid ID { get; set }
+    public Guid StudentID { get; set; } 
+    public Guid CourseID { get; set; } 
+    public int Grade { get; set; } 
+
+    public Grade()
+	{
+	}
+    public Grade(Guid id, Guid studentID, Guid courseID, int grade)
+    {
+        ID = id;
+        studentID = studentID;
+        CourseID = courseID;
+        Grade = grade;
+
+    }
+
+}
