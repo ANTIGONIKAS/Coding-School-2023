06b92a875fa7274732086ad340c5b6610e63c64d
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-16 17:53:49 +0200

    professor class

---
+++ Session-06/Session-06/Professor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Professor : Person
+{   //properties
+    public string Rank { get; set; }
+    public Courses[] Courses { get; set; }
+
+    public Professor()
+    {
+    }
+
+    public Professor(Guid id, string name) : base(id, name)
+    {
+
+    }
+    public Professor(Guid id, string name, int age) : base(id, name, age)
+    {
+
+    }
+
+    public Professor(Guid id, string name, int age, string rank, Courses[] courses) : base(id, name, age)
+    {
+        Rank = rank;
+        Courses = courses;
+
+
+    }
+    //methods
+    public void Teach(Course course, DateTime datetime)
+    {
+
+    }
+    public void SetGrade(Guid studentID, Guid courseID, int grade)
+    {
+
+    }
+
+    public void GetName()
+    { 
+    }
+
+}
