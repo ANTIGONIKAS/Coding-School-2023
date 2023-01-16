558b6723af8f368034593d70580512f0f846bf3f
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-16 17:54:10 +0200

    institute class

---
+++ Session-06/Session-06/Institute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Institute
+{   // properties
+    public Guid ID { get; set }
+    public string Name { get; set; }
+    public int YearsInService { get; set; }
+
+    public Institute()
+    {
+
+    }
+    public Institute(Guid id, string name, int yearsInService)
+    {
+        ID = id;
+        Name = name;
+        YearsInService = yearsInService;
+    }
+
+    public void GetName()
+    {
+    }
+    public void SetName(string name)
+    {
+    }
+}
