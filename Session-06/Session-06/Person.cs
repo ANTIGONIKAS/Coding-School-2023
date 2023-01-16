445e952184812adf3cc86424d0af95cc9898887b
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-16 17:53:59 +0200

    person class

---
+++ Session-06/Session-06/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Person { 
+	  //properties
+	public Guid ID { get;set }
+	public string? Name{ get;set }
+    public int Age { get;set }
+
+	public Person()
+	{
+	}
+	public Person(Guid id) {
+		ID = id;
+	}
+	public Person(Guid id, string name) {
+		ID = id;
+		Name = name;
+	}
+    public Person(Guid id, string name,int age) 
+	{
+            ID = id;
+            Name = name;
+			Age = age;
+
+        }
+	//methods
+	public void GetName()
+	{
+
+	}
+	public void SetName(string name) { 
+		}
+
+}
