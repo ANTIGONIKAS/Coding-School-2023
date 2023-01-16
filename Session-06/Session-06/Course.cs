b08e5ef7485d1de63f04296fbce387eecdc1d9c6
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-16 17:54:56 +0200

    course class

---
+++ Session-06/Session-06/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Course
+{
+    public Guid ID { get; set }
+    public string Code { get; set; }
+    public string Subject { get; set; }
+
+    public Course()
+	{
+	}
+    public Course(Guid id, string code, string subject)
+    {
+        ID = id;
+        Code= code; 
+        Subject= subject;   
+    }
+
+    
+
+}
