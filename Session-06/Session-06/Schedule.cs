d0d75aee7806b4690d1ae7a670aa3ad2ca5ed148
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-16 17:52:55 +0200

    class schedule

---
+++ Session-06/Session-06/Schedule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Schedule
+{
+    public Guid ID { get; set }
+    public Guid CourseID { get; set }
+    public Guid ProfessorID { get; set }
+    public DateTime Callendar { get; set }
+}
+public Schedule()
+	{
+	}
+public Schedule(Guid id,Guid courseID,Guid professorID,DateTime callendar)
+{
+    ID = id;
+    CourseID = courseID;
+    ProfessorID = professorID;
+    DateTime = callendar;
+
+
+}
+
+
+
+
+}
