59de4372884271cefb73ed426d4eb2061df93b0c
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-18 16:23:38 +0200

    stage 2

--- Session-07/Session-07/ActionResolver.cs
+++ Session-07/Session-07/ActionResolver.cs
@@ -7,26 +7,30 @@ using System.Threading.Tasks;
 
 namespace Session_07
 {
-    public enum ActionEnum { 
-    
+    public enum ActionEnum
+    {
+
         Convert,
         Uppercase,
         Reverse,
     }
 
-    public class Message { 
-    
+    public class Message
+    {
+
         public Guid ID { get; set; }
         public DateTime TimeStamp { get; set; }
 
         //Change from Message to Text
         public string Text { get; set; }
     }
-    public class MessageLogger { 
-    
+    public class MessageLogger
+    {
+
         public Message[] Messages { get; set; }
-        public MessageLogger() { 
-        
+        public MessageLogger()
+        {
+
 
         }
         public void ReadAll()
@@ -40,49 +44,77 @@ namespace Session_07
         {
 
         }
-    public class ActionRequest { 
-        
-         public Guid RequestID { get; set; }
-         public String Input { get; set; }
-         public ActionEnum ActionEnum { get; set; }   
+
+        public class ActionRequest
+        {
+
+            public Guid RequestID { get; set; }
+            public String Input { get; set; }
+            public ActionEnum Action { get; set; }
 
 
-         public ActionRequest() { 
-            
-            }
-    public class ActionRespose
+            public ActionRequest()
             {
 
-         public Guid RequestID { get; set; }
-         public Guid ResponseID { get; set; }
-         public String Output { get; set; }
-         public ActionRespose() { 
-                
+            }
+            public class ActionResponse
+            {
 
-         }
-         }
-    public class ActionResolver { 
-        
-        public MessageLogger Logger { get; set; }
+                public Guid RequestID { get; set; }
+                public Guid ResponseID { get; set; }
+                public String Output { get; set; }
+                public ActionResponse()
+                {
 
 
-    public ActionResolver() { 
-                
+                }
+            }
+            public class ActionResolver
+            {
+
+                public MessageLogger Logger { get; set; }
+
+
+                public ActionResolver()
+                {
+
                 }
 
-    public ActionRespose Execute(Action request) {
+                public ActionResponse Execute(ActionRequest request)
+                {
+
+                    switch (request.Action)
+                    {
+                        case ActionEnum.Convert:
+                            Convert();
+                            break;
+                        case ActionEnum.Uppercase:
+                            Uppercase();
+                            break;
+                        case ActionEnum.Reverse:
+                            Reverse();
+                            break;
+                        default:
+                            //Error message here
+                            break;
 
 
+                    }
 
-           return null;        
-           
+
+                    return null;
+
+                }
+
+                public void Convert() { }
+
+                public void Uppercase() { }
+
+                public void Reverse() { }
+
+            }
         }
 
-
-       }
-
-     }
-  }
+    }
 }
-    
 
--- Session-07/Session-07/Program.cs
+++ Session-07/Session-07/Program.cs
@@ -1,11 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Session_07;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Session_07.MessageLogger;
+using static Session_07.MessageLogger.ActionRequest;
 
-Console.WriteLine("Hello, World!");
+namespace Session_07;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ActionRequest request = new ActionRequest();
+        ActionResponse response = new ActionResponse();
 
-//ActionRequest request = new ActionRequest();
-//ActionResponse response = new ActionResponse();
+        ActionResolver resolver = new ActionResolver();
 
-//ActionResolver resolver = new ActionResolver();
+        response = resolver.Execute(request);
 
-////response = resolver.Execute(request);
+
+
+    }
+}
