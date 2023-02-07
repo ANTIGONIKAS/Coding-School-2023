9fdf97b7583c4eae568f3c1469abd61c9e213444
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-02-08 01:49:25 +0200

    session-16  8/2 1:49 pm

---
+++ Session-16/Orm/Repositories/EmployeeRepo.cs
@@ -0,0 +1,64 @@
+﻿using Model;
+using Orm.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orm.Repositories
+{
+    public class EmployeeRepo : IEntityRepo<Employee>
+    {
+        public void Add(Employee entity)
+        {
+            using var context = new PetShopContext();
+            context.Add(entity);
+            context.SaveChanges();
+
+        }
+
+        public void Delete(Guid id)
+        {
+
+            using var context = new PetShopContext();
+            var dbEmployee = context.Employees.Where(employee => employee.EmployeeID == id).SingleOrDefault();
+            if (dbEmployee is null)
+                return;
+            context.Remove(dbEmployee);
+            context.SaveChanges();
+        }
+
+        public void GetAll(Guid id)
+        {
+            
+        }
+        public IList<Employee> GetAll()
+        {
+            using var context = new PetShopContext();
+            return context.Employees.ToList();
+        }
+
+        public Employee? GetById(Guid id)
+        {
+            using var context = new PetShopContext();
+            return context.Employees.Where(employee => employee.EmployeeID == id).SingleOrDefault();
+
+        }
+
+        public void Update(Guid id, Employee entity)
+        {
+            using var context = new PetShopContext();
+            var dbEmployee = context.Employees.Where(employee => employee.EmployeeID == id).SingleOrDefault();
+            if (dbEmployee is null)
+                return;
+            dbEmployee.EmployeeName = entity.EmployeeName;
+            dbEmployee.EmployeeSurname = entity.EmployeeSurname;
+            dbEmployee.SalaryPerMonth = entity.SalaryPerMonth;
+            dbEmployee.EmployeeType = entity.EmployeeType;
+
+            context.Remove(dbEmployee);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Session-16/Orm/Repositories/PetRepo.cs
@@ -0,0 +1,66 @@
+﻿using Model;
+using Orm.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orm.Repositories
+{
+    public  class PetRepo : IEntityRepo<Pet>
+    {
+        public void Add(Pet entity)
+        {
+
+            using var context = new PetShopContext();
+            context.Add(entity);
+            context.SaveChanges();
+
+        }
+
+        public void Delete(Guid id)
+        {
+            using var context = new PetShopContext();
+            var dbPet = context.Pets.Where(pet => pet.PetID== id).SingleOrDefault();
+            if (dbPet is null)
+                return;
+            context.Remove(dbPet);
+            context.SaveChanges();
+
+        }
+        public IList<Pet> GetAll()
+        {
+            using var context = new PetShopContext();
+            return context.Pets.ToList();
+        }
+
+        public void GetAll(Guid id)
+        {
+         
+        }
+
+        public Pet? GetById(Guid id)
+        {
+            using var context = new PetShopContext();
+            return context.Pets.Where(pet => pet.PetID == id).SingleOrDefault();
+        }
+
+        public void Update(Guid id, Pet entity)
+        {
+            using var context = new PetShopContext();
+            var dbPet = context.Pets.Where(pet => pet.PetID == id).SingleOrDefault();
+            if (dbPet is null)
+                return ;
+            dbPet.Breed = entity.Breed;
+            dbPet.Price=entity.Price;
+            dbPet.PetStatus= entity.PetStatus;
+            dbPet.PetType = entity.PetType;
+            dbPet.Cost = entity.Cost;
+
+            context.Remove(dbPet);
+            context.SaveChanges();
+        }
+    }
+    }
+}
